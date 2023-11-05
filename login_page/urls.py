cc0b0402eebf2398a08cf104bd4e5069e76b4940
anas <ansmuhd01@gmail.com>
2023-11-05 14:31:52 +0530

    url change

--- login_page/urls.py
+++ login_page/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("/register", views.user_reg, name="register"),
-    path('/profile', views.profile, name='profile'),
-    path('/login', views.user_login, name='login')
+    path("/register/", views.user_reg, name="register"),
+    path('/profile/', views.profile, name='profile'),
+    path('/login/', views.user_login, name='login')
 ]
