c6bdb9aed29a51ec1401dbf5648477b791454635
anas <ansmuhd01@gmail.com>
2023-11-07 14:45:13 +0530

    form and url update

--- login_page/forms.py
+++ login_page/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-
+from .models import ItemModel
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(max_length=100, help_text='required')
 
@@ -14,3 +14,11 @@ class LoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ['username', 'password']
+
+
+class ItemModelForm(forms.ModelForm):
+    class Meta:
+        model = ItemModel
+        fields = ['title', 'image', 'description', 'price', 'product_type']
+
+    description = forms.CharField(widget=forms.Textarea(attrs={'rows': 4, 'cols': 15}))
