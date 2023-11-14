1814cc0ef60b5b1607cf438434893dccb251eea1
anas <ansmuhd01@gmail.com>
2023-11-14 13:00:12 +0530

    CARTMODEL created

--- login_page/models.py
+++ login_page/models.py
@@ -25,3 +25,14 @@ class ItemModel(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField(ItemModel, through='CartItem')
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    item = models.ForeignKey(ItemModel, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
